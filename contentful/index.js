7e0d4e1a969870a54bb754be9821fcd8b8b4e9a5
Lyshka <53864647+Lyshka@users.noreply.github.com>
2022-09-22 15:10:32 +0300

    add
    add data from cms

---
+++ contentful/index.js
@@ -0,0 +1,8 @@
+import { createClient } from "contentful";
+
+const client = createClient({
+    space: "72v9h5x0hww7",
+    accessToken: "gzFxBkqf9kAi5CckI-QQHwB9ryE6YZ44Bv1azGC5iU4"
+})
+
+export default client
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,47 +2,69 @@ import { BlogCard, ExternalLink, GridContainer, HeaderThree, Hr, Tag, TagList, T
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 import Image from 'next/image';
+import client from "../../../contentful"
+import { useEffect, useState } from 'react';
 
-const Projects = () => (
-	<Section nopadding id='projects'>
-		<SectionDivider />
-		<SectionTitle main>Projects</SectionTitle>
-		<GridContainer>
-			{projects.map(({id, image, title, tags, source, visit}) => (
-				<BlogCard key={id}>
-					<ImgContainer>
-						<Image 
-							src={image}
-							width={"100%"}
-							height={"100%"}
-							layout="responsive"
-							objectFit={"contain"}
-						/>
-					</ImgContainer>
-					<TitleContent>
-						<HeaderThree>{title}</HeaderThree>
-						<Hr />
-					</TitleContent>
-					<div>
-						<TitleContent>Stack: </TitleContent>
-						<TagList>
-							{tags.map((tag, i) => (
-								<Tag key={i}>{tag}</Tag>
-							))}
-						</TagList>
-					</div>
-					<UtilityList>
-						<ExternalLink href={visit} target="_blank">
-							Code
-						</ExternalLink>
-						<ExternalLink href={source} target="_blank">
-							Source
-						</ExternalLink>
-					</UtilityList>
-				</BlogCard>
-			))}
-		</GridContainer>
-	</Section>
-);
+const Projects = () => {
+	const [projects2, setPropjects] = useState([])
+
+	const getProjects = async () => {
+		const projectsObj = await client.getEntries({
+			content_type: "projects"
+		})
+
+		const projects = projectsObj.items.map(({fields}) => fields)
+
+		setPropjects(projects)
+	}
+
+	useEffect(() => {
+		getProjects()
+	}, [])
+
+	// console.log(projects2[0].image)
+
+	return (
+		<Section nopadding id='projects'>
+			<SectionDivider />
+			<SectionTitle main>Projects</SectionTitle>
+			<GridContainer>
+				{projects2?.map(({ image, title, tags, source, visit }) => (
+					<BlogCard key={title}>
+						<ImgContainer>
+							<Image
+								src={`https:${image.fields.file.url}`}
+								width={"100%"}
+								height={"100%"}
+								layout="responsive"
+								objectFit={"contain"}
+							/>
+						</ImgContainer>
+						<TitleContent>
+							<HeaderThree>{title}</HeaderThree>
+							<Hr />
+						</TitleContent>
+						<div>
+							<TitleContent>Stack: </TitleContent>
+							<TagList>
+								{tags.map((tag, i) => (
+									<Tag key={i}>{tag}</Tag>
+								))}
+							</TagList>
+						</div>
+						<UtilityList>
+							<ExternalLink href={visit} target="_blank">
+								Source
+							</ExternalLink>
+							<ExternalLink href={source} target="_blank">
+								Code
+							</ExternalLink>
+						</UtilityList>
+					</BlogCard>
+				))}
+			</GridContainer>
+		</Section>
+	)
+};
 
 export default Projects;
